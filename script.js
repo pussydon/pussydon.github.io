3726f3210001e000dcc65cbbf2b847da49cbf178
pussydon <cancersuit@mail.ru>
2019-12-05 14:04:56 +0300

    sth

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-'use strict'
-
-let container = document.getElementById('container')
-let btnLeft = document.getElementById('btnLeft')
-let btnRight = document.getElementById('btnRight')
-let num = 0
-
-btnRight.addEventListener('click', function(){
-    if(num < 3){
-        container.classList.add('pos' + (num + 1))
-        container.classList.remove('pos' + num)
-        num += 1
-    } else{
-        container.classList.add('pos' + 1)
-        container.classList.remove('pos' + num)
-        num = 1
-    }
-})
-btnLeft.addEventListener('click', function(){
-    if(num >= 1){
-        container.classList.add('pos' + (num - 1))
-        container.classList.remove('pos' + num)
-        num -= 1
-    } else{
-        container.classList.add('pos' + 2)
-        container.classList.remove('pos' + num)
-        num = 2
-    }
-})
+$(document).ready(function(){
+    $('#menu').on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
