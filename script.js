b164176f2fd2f3c27eacc177751bc151e42c5bdb
pussydon <cancersuit@mail.ru>
2019-12-08 01:28:52 +0300

    sth

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 
         let count = Math.floor(Math.random() * (1000 - 500)) + 500
+        let container = document.querySelector('.container')
+
         for (let i = 1; i <= count; i++){
             let el = document.createElement('div')
             el.classList.add('button')
